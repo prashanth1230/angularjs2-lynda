c3dc97fa4bfe68c7500028cc0b5e1a2e2075d764
Udupa, Prashanth <prashanth.udupa@fmr.com>
2016-12-12 17:33:37 +0530

    Using the styleUrls to attach style files.

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,15 +3,7 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'mw-app',
     templateUrl: 'app/app.component.html',
-    styles: [`
-    h1 {
-       color: #ffffff;
-    }
-    .description {
-       font-style: italic;
-       color: green;
-    }
-`]
+    styleUrls: ['app/app.component.css']
 })
 export class AppComponent {
 }
