23dc8dd3f528728bade158054c183665cf2b2cb6
Udupa, Prashanth <prashanth.udupa@fmr.com>
2016-12-15 18:48:51 +0530

    Adding a HostBinding to add custom directive binding functions. Also add HoseListners to listen to events on the HTML DOM.

--- app/favourite.directive.ts
+++ app/favourite.directive.ts
@@ -1,8 +1,18 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
     selector: '[mwFavourite]'
 })
 export class FavouriteDirective {
-    @HostBinding('class.is-favourite') isFavourite = true;
+    @HostBinding('class.is-favourite') isFavourite;
+    @HostBinding('class.is-favourite-hovering') hovering = false;
+    @HostListener('mouseenter') onMouseEnter() {
+        this.hovering = true;
+    }
+    @HostListener('mouseleave') onMouseLeave() {
+        this.hovering = false;
+    }
+    @Input() set mwFavourite(value) {
+        this.isFavourite = value;
+    }
 }
